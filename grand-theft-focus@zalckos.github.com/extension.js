43f60b93e9822fc6c51e34da4bb28611a470ca9e
Tiago de Paula <tiagodepalves@gmail.com>
2026-03-07 22:43:26 -0300

    Fix extension not properly disabled
    Instead of relying on 'destroy' being called, just inline used methods
    directly.

--- grand-theft-focus@zalckos.github.com/extension.js
+++ grand-theft-focus@zalckos.github.com/extension.js
@@ -1,35 +1,18 @@
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as WindowAttentionHandler from 'resource:///org/gnome/shell/ui/windowAttentionHandler.js';
-import Shell from 'gi://Shell';
 
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-
-function GrandTheftFocus() {
-    this._init();
-}
-
-GrandTheftFocus.prototype = {
-    _init : function() {
-        this._tracker = Shell.WindowTracker.get_default();
-        this._handlerid = global.display.connect('window-demands-attention', this._onWindowDemandsAttention.bind(this));
-    },
-
-    _onWindowDemandsAttention: function(display, window) {
-        if (!Main.overview._shown)
-            Main.activateWindow(window);
-    },
-
-    destroy: function () {
-        global.display.disconnect(this._handlerid);
+function onWindowDemandsAttention(display, window) {
+    if (!Main.overview._shown) {
+        Main.activateWindow(window);
     }
 }
 
 export default class GrandTheftExtension {
     enable() {
-        this._grandtheftfocus = new GrandTheftFocus();
+        this._handlerid = global.display.connect('window-demands-attention', onWindowDemandsAttention);
     }
 
     disable() {
-        this._grandtheftfocus = null;
+        global.display.disconnect(this._handlerid);
+        delete this._handlerid;
     }
 }
